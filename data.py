5e269cb938ceebbcd945a83854f4420df9f48637
Anastasiya18-maker <achernyshina@yandex.ru>
2025-02-27 20:02:58 +0400

    New_commit

---
+++ data.py
@@ -0,0 +1,13 @@
+import random
+
+class PersonData:
+    user_name = 'Анастасия'
+    login = 'chernyshina15fs@yandex.ru'
+
+    password = '18031993'
+ #ValidData
+class ValidData:
+    user_name = 'Анастасия'
+    login = 'chernyshina15fs@yandex.ru'
+
+    password = '18031993'
---
+++ tests/test_login.py
@@ -0,0 +1,81 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+from urls import Urls
+from data import PersonData
+
+from locators import MainPage, AuthLogin, AuthPassword
+
+
+class TestStellarBurgersLoginLogoutForm:
+
+    def test_login_correct_email_and_password_show_main_page(self, login):
+        # При вводе корректных данных основная страница
+
+        WebDriverWait(login, 5).until(EC.presence_of_element_located(MainPage.mn_order_button))
+        order_button = login.find_element(*MainPage.mn_order_button)
+        assert login.current_url == Urls.url_main_paige and order_button.text == 'Оформить заказ'
+
+    def test_login_sign_in_button_show_login_page(self, driver):
+        # Вход по кнопке 'Войти в аккаунт'
+
+        driver.find_element(*MainPage.mn_auth).click()
+        WebDriverWait(driver, 8).until(EC.presence_of_element_located(AuthLogin.al_login_text))
+
+        driver.find_element(*AuthLogin.al_email_field).send_keys(PersonData.login)
+        driver.find_element(*AuthLogin.al_password_field).send_keys(PersonData.password)
+
+        driver.find_element(*AuthLogin.al_login_button_any_forms).click()
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located(MainPage.mn_order_button))
+
+        order_button = driver.find_element(*MainPage.mn_order_button)
+        assert driver.current_url == Urls.url_main_paige and order_button.text == 'Оформить заказ'
+
+    def test_login_personal_account_button_show_login_page(self, driver):
+        # Вход через кнопку 'Личный Кабинет'
+
+        driver.find_element(*MainPage.mn_profile_button).click()
+        WebDriverWait(driver, 8).until(EC.presence_of_element_located(AuthLogin.al_login_text))
+
+        driver.find_element(*AuthLogin.al_email_field).send_keys(PersonData.login)
+        driver.find_element(*AuthLogin.al_password_field).send_keys(PersonData.password)
+
+        driver.find_element(*AuthLogin.al_login_button_any_forms).click()
+        WebDriverWait(driver, 8).until(EC.presence_of_element_located(MainPage.mn_order_button))
+
+        order_button = driver.find_element(*MainPage.mn_order_button)
+        assert driver.current_url == Urls.url_main_paige and order_button.text == 'Оформить заказ'
+
+    def test_login_registration_form_sign_in_button(self, driver):
+        # Вход через кнопку 'Войти' в форме регистрации
+        driver.get(Urls.url_register)
+
+        driver.find_element(*AuthLogin.al_login_text_with_href).click()
+        WebDriverWait(driver, 8).until(EC.presence_of_element_located(AuthLogin.al_login_text))
+
+        driver.find_element(*AuthLogin.al_email_field).send_keys(PersonData.login)
+        driver.find_element(*AuthLogin.al_password_field).send_keys(PersonData.password)
+
+        driver.find_element(*AuthLogin.al_login_button_any_forms).click()
+        WebDriverWait(driver, 8).until(EC.presence_of_element_located(MainPage.mn_order_button))
+
+        order_button = driver.find_element(*MainPage.mn_order_button)
+        assert driver.current_url == Urls.url_main_paige and order_button.text == 'Оформить заказ'
+
+    def test_login_forgot_password_form_sign_in_button(self, driver):
+        # Вход через кнопку 'Войти' в форме 'Восстановление пароля'
+        driver.get(Urls.url_forgot_password)
+
+        driver.find_element(*AuthPassword.ap_login_text_with_href).click()
+        WebDriverWait(driver, 8).until(EC.presence_of_element_located(AuthLogin.al_login_text))
+
+        driver.find_element(*AuthLogin.al_email_field).send_keys(PersonData.login)
+        driver.find_element(*AuthLogin.al_password_field).send_keys(PersonData.password)
+
+        driver.find_element(*AuthLogin.al_login_button_any_forms).click()
+        WebDriverWait(driver, 8).until(EC.presence_of_element_located(MainPage.mn_order_button))
+
+        order_button = driver.find_element(*MainPage.mn_order_button)
+        assert driver.current_url == Urls.url_main_paige and order_button.text == 'Оформить заказ'
+
